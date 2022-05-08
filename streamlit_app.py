2957d493fc8938171a468ae8967dd6ec258a2f95
Cedric K. Wachira <41835548+cedric-kiama-wachira@users.noreply.github.com>
2022-05-08 12:30:16 +0400

    Update streamlit_app.py
    Fixed a typo
    Added one header
    Added three text containing menu Items - Omega3... Kale... Hard-Boiled....

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parent\'s new healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
