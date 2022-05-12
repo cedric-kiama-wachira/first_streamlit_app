e9a765ca3f6be8bd3c86edf7a196af1e944208ef
Cedric K. Wachira <41835548+cedric-kiama-wachira@users.noreply.github.com>
2022-05-12 09:58:23 +0400

    Update streamlit_app.py
    Added a text entry and send the input to fruityvice as Part of the API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,8 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
+streamlit.write('The user entered', fruit_choice)
 
 # New Section to Display fruitvice api response
 import requests
